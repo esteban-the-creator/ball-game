fe67c7e22068ae8dee466202a65b857f08e60790
esteban-the-creator <42298103+esteban-the-creator@users.noreply.github.com>
2019-08-03 11:10:54 -0500

    First Commit
    begin

---
+++ physics/Assets/Scripts/Sphere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+  
+
+
+
+
+
+}
+
+/*
+ que una gravedad lo tire al piso 
+ que al ar click salte en parabola contra la direccion de la gravedad
+ que los chuzos hagan que se pierda
+ que haya algun elemento que se deba coger como puntos
+ que aparecan chuzos laterales aleatorios en posiciones diferentes a las de los puntos que se deben coger 
+
+     */
