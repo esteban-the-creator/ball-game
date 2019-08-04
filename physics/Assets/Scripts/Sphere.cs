26ee8c03a6b572d72dc3686482dc71d9e4bec384
esteban-the-creator <estebangs98@gmail.com>
2019-08-04 16:13:12 -0500

    adds sprites
    1

--- physics/Assets/Scripts/Sphere.cs
+++ physics/Assets/Scripts/Sphere.cs
@@ -6,11 +6,11 @@ public class Sphere : MonoBehaviour
 {
     public float forceUP = 5;
     public float moveX = 2.5f;
-    Rigidbody2D PlayerRgdBdy;
+    private Rigidbody2D PlayerRgdBdy;
 
     private void Start()
     {
-        PlayerRgdBdy = GetComponent<Rigidbody2D>();
+        PlayerRgdBdy = GetComponent<Rigidbody2D>(); // a la variable rigidbody creada se le asigna el componente RigidBody2D del objeto al que se le asigne
     }
 
     private void Update()
@@ -19,19 +19,17 @@ public class Sphere : MonoBehaviour
         {
             Movement();
         }
-
-        
     }
 
     public void Movement()
     {
-        PlayerRgdBdy.velocity = new Vector2(moveX, forceUP);
-
+        PlayerRgdBdy.velocity = new Vector2(moveX, forceUP); // velocity es una propiedad de la clase Rigidbody que permite modificar la posicion del transform
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) /* una funcion reservada que se ejecuta sola sin la necesidad de ser llamada en un Update que permite
+       detectar cuándo se colisiona contra un objeto */
     {
-        moveX *= -1;
+        moveX *= -1; // la variable moveX sera igual a el valor que tenga moveX en ese momento por -1 para cambiar la magnitud de su dirección
     }
 
 
@@ -41,8 +39,8 @@ public class Sphere : MonoBehaviour
  * 
  * rigibdoy.velocity y .addforce
  * use movetowards
- que una gravedad lo tire al piso 
- que al ar click salte en parabola contra la direccion de la gravedad
+ que una gravedad lo tire al piso Y
+ que al ar click salte en parabola contra la direccion de la gravedadY
  que los chuzos hagan que se pierda
  que haya algun elemento que se deba coger como puntos
  que aparecan chuzos laterales aleatorios en posiciones diferentes a las de los puntos que se deben coger 
